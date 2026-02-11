6170a78bcd6c1982adfb4bb8fb22e177957b37fb
Setoju <setoju48@gmail.com>
2026-02-11 09:28:12 +0200

    feat: Add default scopes

--- app/models/lecture_question.rb
+++ app/models/lecture_question.rb
@@ -4,4 +4,6 @@ class LectureQuestion < ApplicationRecord
     validates :question_text, presence: true
     validates :answer_text, presence: true
     validates :position, numericality: { greater_than_or_equal_to: 0 }
+
+    default_scope { order(:position) }
 end
--- app/models/lesson_ai_summary.rb
+++ app/models/lesson_ai_summary.rb
@@ -3,4 +3,6 @@ class LessonAiSummary < ApplicationRecord
     
     validates :summary_text, presence: true
     validates :chunk_index, numericality: { greater_than_or_equal_to: 0 }
+
+    default_scope { order(:chunk_index) }
 end
